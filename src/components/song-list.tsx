3a563955ad3121ebf74a15d52d174203d92047e0
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-10 17:42:02 -0300

    feat(components): add song-list

---
+++ src/components/song-list.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Link from "next/link";
+
+export interface SongData {
+  _id: string;
+  name: string;
+  slug: string;
+}
+
+export interface Saga {
+  _id: string;
+  name: string;
+  songs: SongData[];
+}
+
+interface SongListProps {
+  sagas: Saga[];
+}
+
+const textColors: Record<string, string> = {
+  "Troy Saga": "text-red-400",
+  "Polyphemus Saga": "text-orange-400",
+  "Ocean Saga": "text-blue-400",
+  "Circe Saga": "text-fuchsia-400",
+  "Underworld Saga": "text-gray-400",
+  "Thunder Saga": "text-yellow-400",
+};
+
+export async function SongList({ sagas }: SongListProps) {
+  return (
+    <ul className="flex flex-col items-start gap-4">
+      {sagas.map((saga) => (
+        <li key={saga._id} className="space-y-1">
+          <span className="font-medium text-lg text-zinc-100">{saga.name}</span>
+
+          <ul className="ml-4">
+            {saga.songs.map((song) => (
+              <li
+                key={song._id}
+                className={`${textColors[saga.name]} text-opacity-50 hover:text-opacity-100`}
+              >
+                <Link href={`/songs/${song.slug}`}>
+                  <span>{song.name}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+  );
+}

4b6f3da89da11d0e60875fc83e70b645b29583ad
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-10 17:46:33 -0300

    feat(pages): add dynamic song route with SSG enabled

---
+++ src/app/songs/[slug]/page.tsx
@@ -0,0 +1,48 @@
+import { SongLayout } from "@/components/song-layout";
+import { SongData } from "@/components/song-list";
+import { client } from "@/sanity/lib/client";
+import { PortableTextBlock } from "next-sanity";
+
+interface Params {
+  slug: string;
+}
+
+export interface SongWithLyrics extends SongData {
+  saga: string;
+  lyrics: PortableTextBlock;
+}
+
+export async function generateStaticParams() {
+  const slugs = await client.fetch<{ slug: string }[]>(
+    `*[_type == "song"]{ "slug": slug.current }`
+  );
+  return slugs;
+}
+
+export async function generateMetadata({ params }: { params: Params }) {
+  const songNames = await client.fetch<{ name: string; slug: string }[]>(
+    `*[_type == "song"]{ name, "slug": slug.current }`
+  );
+  const song = songNames.find((s) => s.slug == params.slug);
+  return {
+    title: `Epic Translations | ${song?.name}`,
+    description: `Leia a letra traduzida de ${song?.name}.`,
+  };
+}
+
+export const dynamicParams = false;
+
+async function fetchSong(slug: string) {
+  const song = await client.fetch<SongWithLyrics>(`*[_type == "song" && slug.current == "${slug}" ]{
+      name,
+      "saga": saga->name,
+      lyrics
+    }[0]`);
+  return song;
+}
+
+export default async function Song({ params }: { params: Params }) {
+  const song = await fetchSong(params.slug);
+
+  return <SongLayout song={song} />;
+}

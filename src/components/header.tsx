66e5b5a9de0537b5d718d3f2ca9078c19f9bb239
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-10 17:46:04 -0300

    refactor(components/header.tsx): use greek-temple.svg as homepage link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export function Header() {
   return (
     <header className="w-full flex items-center justify-center py-4 text-zinc-100 bg-zinc-900 shadow-lg">
       <Link href="/">
-        <span className="font-medium text-lg">Epic Translations</span>
+        <Image src="/greek-temple.svg" width={32} height={32} alt="Greek temple icon" />
       </Link>
     </header>
   );

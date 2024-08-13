0af3d930788f68e6733a9dc1b3fc9597d08135e1
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-13 10:31:15 -0300

    chores: test webhook in prod xd

--- src/app/api/revalidate-update/route.ts
+++ src/app/api/revalidate-update/route.ts
@@ -1,6 +1,7 @@
-export async function GET(request: Request) {
+export async function POST(request: Request) {
   try {
     const text = await request.text();
+    console.log(request.headers);
     console.log(text);
   } catch (error) {
     return new Response("Error", { status: 400 });

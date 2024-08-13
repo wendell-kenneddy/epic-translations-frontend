9f289834bff81e51e4d4451e25e44e193e610c01
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-13 10:49:45 -0300

    feat(api/revalidate-update): test for webhook signature

--- src/app/api/revalidate-update/route.ts
+++ src/app/api/revalidate-update/route.ts
@@ -1,8 +1,20 @@
-export async function POST(request: Request) {
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
+
+export async function POST(req: Request) {
   try {
-    const text = await request.text();
-    console.log(request.headers);
-    console.log(text);
+    const text = await req.text();
+    const signature = req.headers.get(SIGNATURE_HEADER_NAME);
+    if (
+      !(await isValidSignature(
+        text,
+        String(signature),
+        String(process.env.SANITY_UPDATE_WEBHOOK_SECRET)
+      ))
+    ) {
+      return new Response(JSON.stringify({ success: false, message: "Invalid signature" }), {
+        status: 401,
+      });
+    }
   } catch (error) {
     return new Response("Error", { status: 400 });
   }

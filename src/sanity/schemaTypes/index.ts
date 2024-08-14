a57072a03446d7d3402eb8fef3e2090f53d5cabe
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-14 20:05:30 -0300

    refactor: remove sanity from the project

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import { sagaType } from "./sagaType";
-import { songType } from "./songType";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [sagaType, songType],
-};

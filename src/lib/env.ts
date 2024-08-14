c27de737c7466bbe08a54f8001014a166b9771e4
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-14 20:06:37 -0300

    feat(app/lib): add env wrapper

---
+++ src/lib/env.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  CONTENTFUL_SPACE_ID: z.string(),
+  CONTENTFUL_ACCESS_TOKEN: z.string(),
+  CONTENTFUL_API_URL: z.string(),
+  BASE_URL: z.string(),
+});
+
+export const env = envSchema.parse(process.env);

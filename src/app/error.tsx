dc3042db602eae402313dbf72f86dd64d6be38d9
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-14 20:06:14 -0300

    feat(pages): add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="w-[90%] max-w-[500px] mx-auto flex items-center justify-center">
+      <h1 className="text-lg text-slate-100">Something went wrong...</h1>
+    </main>
+  );
+}

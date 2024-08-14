597cc80d2ed20758b80ca3ac33f140e435c3cad6
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-14 20:07:07 -0300

    feat(queries): add fetch-song

---
+++ src/queries/fetch-song.ts
@@ -0,0 +1,19 @@
+import { Document } from "@contentful/rich-text-types";
+import { env } from "@/lib/env";
+import { fetchSongs } from "./fetch-songs";
+
+export async function fetchSong(slug: string) {
+  const json = await fetchSongs();
+  const songID = String(json.items.find((s) => s.fields.slug == slug)?.sys.id);
+  const songResponse = await fetch(
+    `${env.CONTENTFUL_API_URL}/entries/${songID}?access_token=${env.CONTENTFUL_ACCESS_TOKEN}&select=fields.name,fields.lyrics`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      next: { tags: [`song-${slug}`] },
+    }
+  );
+  const songJson: { fields: { name: string; lyrics: Document } } = await songResponse.json();
+  return songJson;
+}

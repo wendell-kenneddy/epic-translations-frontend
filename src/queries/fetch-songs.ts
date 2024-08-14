3136ef10f315e80e5cf227d700257339e32e3efa
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-14 20:06:52 -0300

    feat(queries): add fetch-songs

---
+++ src/queries/fetch-songs.ts
@@ -0,0 +1,38 @@
+import { env } from "@/lib/env";
+
+export interface SongsAndSagasResponse {
+  items: {
+    sys: {
+      id: string;
+    };
+    fields: {
+      name: string;
+      slug: string;
+      saga: {
+        sys: {
+          id: string;
+        };
+      };
+    };
+  }[];
+  includes: {
+    Entry: {
+      sys: { id: string };
+      fields: { name: string };
+    }[];
+  };
+}
+
+export async function fetchSongs() {
+  const response = await fetch(
+    `${env.CONTENTFUL_API_URL}entries?access_token=${env.CONTENTFUL_ACCESS_TOKEN}&content_type=song&select=sys.id,fields.name,fields.slug,fields.saga&order=sys.createdAt&include=1`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+    }
+  );
+  const json: SongsAndSagasResponse = await response.json();
+  return json;
+}

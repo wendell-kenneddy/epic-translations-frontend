6ebfc869fd402e50d0afebaf9f44e9c77d6f97e1
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-08-15 09:29:48 -0300

    feat(env): add CONTENTFUL_REVALIDATE_SECRET

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   CONTENTFUL_SPACE_ID: z.string(),
   CONTENTFUL_ACCESS_TOKEN: z.string(),
   CONTENTFUL_API_URL: z.string(),
+  CONTENTFUL_REVALIDATE_SECRET: z.string(),
   BASE_URL: z.string(),
 });
 

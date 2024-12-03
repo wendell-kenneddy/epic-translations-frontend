2bc12516f5f87f151503994f19171b9b1bfda994
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-03 13:02:04 -0300

    feat(components/header): update styles and include children prop

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+import { ReactNode } from "react";
 
-export function Header() {
+interface HeaderProps {
+  children: ReactNode;
+}
+
+export function Header({ children }: HeaderProps) {
   return (
-    <header className="w-full flex items-center justify-center py-4 text-slate-100 bg-slate-900 shadow-lg">
-      <Link href="/">
-        <Image src="/greek-temple.svg" width={32} height={32} alt="Greek temple icon" />
-      </Link>
+    <header className="w-full h-[10vh] flex items-center justify-center py-4 text-slate-100 bg-slate-900 shadow-lg border-b border-slate-800">
+      <div className="w-[90%] max-w-[500px] flex items-center justify-between sm:justify-center">
+        {children}
+
+        <Link href="/">
+          <Image src="/greek-temple.svg" width={32} height={32} alt="Greek temple icon" />
+        </Link>
+      </div>
     </header>
   );
 }

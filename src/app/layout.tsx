88dd7d2a0a1c145bc28c0036b0e1d20057f83cad
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-03 16:38:14 -0300

    feat(layout): add seo related meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,25 @@
 import { Footer } from "@/components/footer";
 import { MainNavigation } from "@/components/main-navigation";
 import "@/styles/globals.css";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ReactNode } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 interface RootLayoutProps {
   children: ReactNode;
 }
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  creator: "Wendell Kenneddy",
+  icons: [
+    { rel: "icon", url: "/favicon.ico", sizes: "any" },
+    { rel: "icon", url: "/icon.svg", type: "image/svg+xml" },
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png", type: "image/png" },
+  ],
+  other: { "theme-color": "#0f172a" },
+};
 
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (

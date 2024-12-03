342b93d1dff0d4e7c4f590814632dd86b173a3b9
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-03 16:38:35 -0300

    feat(pages/chuulia): make chuu's gif screen size

--- src/app/chuulia/page.tsx
+++ src/app/chuulia/page.tsx
@@ -2,16 +2,15 @@ import Image from "next/image";
 
 export default function Chuulia() {
   return (
-    <main className="w-full h-full flex flex-col items-center p-4 space-y-4 rounded-md shadow-lg">
-      <p className="text-slate-100 font-medium">live julia lana reaction:</p>
-
+    <main className="w-full h-full flex flex-col items-center rounded-md shadow-lg">
       <Image
         priority={true}
         unoptimized={true}
         src="/chuulia.gif"
         alt="Aggressive Chuu gif"
-        width={200}
-        height={200}
+        width={256}
+        height={256}
+        className="w-full h-full"
       />
     </main>
   );
